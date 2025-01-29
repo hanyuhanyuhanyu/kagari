6d97ef14c2dbb16600c5851f795b5f10ab08c9ae
umano <concon.wolf@gmail.com>
2025-01-29 23:11:21 +0900

    display markdown example

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+const apiBase =
+  "https://zoqqfl4b4jblcji34ewncf37fe0mvmem.lambda-url.ap-northeast-1.on.aws";
+const s3Base = "https://kagari-frontend-static.s3.ap-northeast-1.amazonaws.com";
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const urlParams = new URLSearchParams(window.location.search);
+  const getArticleId = () => {
+    const articleId = urlParams.get("article_id");
+    if (typeof articleId !== "string") return;
+    if (articleId.length > 32 || articleId.match(/\D/)) return;
+    return articleId;
+  };
+  const fetchArticle = async () => {
+    const url = await fetch(apiBase, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        articleId: getArticleId() || 1,
+      }),
+    })
+      .then((r) => r.json())
+      .then((r) => r.url);
+    // TODO sanitize
+    return await fetch(`${s3Base}/${url}`).then((r) => r.text());
+  };
+  const [text, setText] = useState("");
+  useEffect(() => {
+    fetchArticle().then(setText);
+  }, []);
+  return <div>{text}</div>;
 }
 
-export default App
+export default App;
