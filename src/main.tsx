652527f95fa4aa72a6eade1b2b9b756bb2987b0b
umano <concon.wolf@gmail.com>
2025-02-12 08:37:42 +0900

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { replace } from "react-router";
 
 const root = createRoot(document.getElementById("root")!);
+// s3 hostingでパスに#!がつくようにしてある
+// 詳しくはここ https://via.studio/journal/hosting-a-reactjs-app-with-routing-on-aws-s3
 const path = (/#!(\/?.*)$/.exec(location.hash) || [])[1];
-if (path) replace(path);
+if (path) history.replaceState(null, "", path);
 root.render(
   <StrictMode>
     <App />
