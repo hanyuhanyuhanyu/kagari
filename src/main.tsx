cfea73511e419b14d9f25e2015bc17370fe423d4
umano <concon.wolf@gmail.com>
2025-02-12 08:10:36 +0900

    s3 static hostingでrouterを機能させたい

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import { replace } from "react-router";
 
-createRoot(document.getElementById("root")!).render(
+const root = createRoot(document.getElementById("root")!);
+const path = (/#!(\/?.*)$/.exec(location.hash) || [])[1];
+if (path) replace(path);
+root.render(
   <StrictMode>
     <App />
   </StrictMode>
