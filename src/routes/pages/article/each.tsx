8a0cad22e2d5e35f3592a521df846165f8780b49
umano <concon.wolf@gmail.com>
2025-02-12 13:31:32 +0900

    mermaid対応

--- src/routes/pages/article/each.tsx
+++ src/routes/pages/article/each.tsx
@@ -1,20 +1,47 @@
 import { useEffect, useState } from "react";
-import Markdown from "react-markdown";
 import RemarkBreaks from "remark-breaks";
 import RemarkEmoji from "remark-emoji";
 import { articleApi } from "../../../external_interface/http/backend/article";
 import { useParams } from "react-router";
+import type { Root } from "mdast";
+import mermaid from "mermaid";
+import { unified } from "unified";
+import remarkRehype from "remark-rehype";
+import rehypeStringify from "rehype-stringify";
+import remarkParse from "remark-parse";
+function VisualizeMermaid() {
+  return async function (tree: Root) {
+    await Promise.all(
+      tree.children.map(async (t, ind) => {
+        if (t.type !== "code" || t.lang !== "mermaid") return;
+        await mermaid.render(`svg-${ind}`, t.value).then((r) => {
+          tree.children[ind] = {
+            type: "html",
+            value: r.svg,
+          };
+        });
+      })
+    );
+  };
+}
 function Article() {
   let { url } = useParams();
   const [text, setText] = useState("");
   useEffect(() => {
     (async () => {
-      await articleApi.getBody(decodeURIComponent(url || "")).then(setText);
+      const text = await articleApi.getBody(decodeURIComponent(url || ""));
+      const uni = await unified()
+        .use(remarkParse)
+        .use(RemarkBreaks)
+        .use(RemarkEmoji)
+        .use(VisualizeMermaid)
+        .use(remarkRehype, { allowDangerousHtml: true }) // TODO: なんとかして
+        .use(rehypeStringify, { allowDangerousHtml: true }) // TODO: なんとかして
+        .process(text);
+      setText(uni.toString());
     })();
   }, []);
-  return (
-    <Markdown remarkPlugins={[RemarkBreaks, RemarkEmoji]}>{text}</Markdown>
-  );
+  return <div dangerouslySetInnerHTML={{ __html: text }} />;
 }
 
 export default Article;
