c889e9d13d6630a8944f60ec59f1e3d0c52b05b9
umano <concon.wolf@gmail.com>
2025-02-20 00:41:02 +0000

    無駄な書き方の抑制

--- src/App.tsx
+++ src/App.tsx
@@ -10,9 +10,7 @@ function App() {
         <Header />
         <div className="px-8 py-4 grid gap-4 grid-rows-12 grid-cols-12 box-border h-full">
           <Articles className="col-span-12 lg:col-span-5 xl:col-span-4 2xl:col-span-3 row-span-3 lg:row-span-12" />
-          <VLinedBox
-            className={`col-span-12 lg:col-span-7 xl:col-span-8 2xl:col-span-9 row-span-9 lg:row-span-12 h-full overflow-y-auto`}
-          >
+          <VLinedBox className="col-span-12 lg:col-span-7 xl:col-span-8 2xl:col-span-9 row-span-9 lg:row-span-12 h-full overflow-y-auto">
             <main className="h-full w-full">
               <RouterDefinition />
             </main>
