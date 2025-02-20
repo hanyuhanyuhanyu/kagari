a07da329c24f4b8ffcc3dd9b6ba615a4eeffa1a3
umano <concon.wolf@gmail.com>
2025-02-20 00:35:25 +0000

    a

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
       <div className="grid grid-rows-[64px_calc(100vh-64px)] overflow-hidden h-full w-full">
         <Header />
         <div className="px-8 py-4 grid gap-4 grid-rows-12 grid-cols-12 box-border h-full">
-          <Articles className="col-span-12 lg:col-span-5 xl:col-span-4 2xlcol-span-3 row-span-3 lg:row-span-12" />
+          <Articles className="col-span-12 lg:col-span-5 xl:col-span-4 2xl:col-span-3 row-span-3 lg:row-span-12" />
           <VLinedBox
             className={`col-span-12 lg:col-span-7 xl:col-span-8 2xl:col-span-9 row-span-9 lg:row-span-12 h-full overflow-y-auto`}
           >
